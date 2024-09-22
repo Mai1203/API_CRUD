aaec8199252688ba9ac984440417cbf345b3d2c1
Mai1203 <83738249+Mai1203@users.noreply.github.com>
2024-09-22 18:52:48 -0500

    models-products

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 // import { Express } from 'express'
 const express = require("express");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const app = express();
-
-app.listen(3000, () => {
-  console.log("Server corriendo en el puerto 3000");
-});
 
 app.get("/", (req, res) => {
   res.send("Hola desde Node API en curso desde vs studio");
 });
 
-mongoose.connect("mongodb+srv://maicolcoralbravo:eWG9Wc0q2mmtys8n@cluster0.y3h8j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+app.post("/api/products", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://maicolcoralbravo:eWG9Wc0q2mmtys8n@cluster0.y3h8j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
-    console.log("OK Conexión exitosa")
+    console.log("OK Conexión exitosa");
+    app.listen(3000, () => {
+      console.log("Server corriendo en el puerto 3000");
+    });
   })
   .catch(() => {
-    console.log("fallo")
-  })
+    console.log("fallo");
+  });
